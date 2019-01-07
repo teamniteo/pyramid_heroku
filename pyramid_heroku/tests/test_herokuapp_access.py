15c067526b366c30ed2f5f3fa71b8862bd394c6f
Igor Kolar <igor.kolar@gmail.com>
2019-01-07 15:06:52 +0000

    Raised code coverage for pyramid_heroku/herokuapp_access.py to 100%.
    Had some issues getting the logging test to work.  In the end, I had to make
    the structlog version log to the standard log, so that zope could still
    catch it's messages.
    
    Ref: #10

--- pyramid_heroku/tests/test_herokuapp_access.py
+++ pyramid_heroku/tests/test_herokuapp_access.py
@@ -3,13 +3,21 @@
 from pyramid import testing
 from zope.testing.loggingsupport import InstalledHandler
 
+import logging
 import mock
 import unittest
+import structlog
 
 tweens_handler = InstalledHandler('pyramid_heroku.herokuapp_access')
 
 
 class TestHerokuappAccessTween(unittest.TestCase):
+
+    def wrap_logger(self, _, __, event_dict):
+        """Have structlog output to the standard logger, so that tweens_handler can intercept its messages."""
+        wrapped_logger = logging.getLogger('pyramid_heroku.herokuapp_access')
+        wrapped_logger.info(event_dict["event"])
+        return event_dict["event"]
 
     def setUp(self):
         tweens_handler.clear()
@@ -17,7 +25,10 @@ class TestHerokuappAccessTween(unittest.TestCase):
         self.handler = mock.Mock()
         self.request = testing.DummyRequest()
         self.request.registry.settings = {
-            'pyramid_heroku.herokuapp_whitelist': ['1.2.3.4', ]}
+            'pyramid_heroku.herokuapp_whitelist': ['1.2.3.4', ],
+            'pyramid_heroku.structlog': 1,
+        }
+        structlog.configure(processors=[self.wrap_logger], context_class=dict)
 
     def tearDown(self):
         tweens_handler.clear()
@@ -40,7 +51,23 @@ class TestHerokuappAccessTween(unittest.TestCase):
         self.request.headers = {
             'Host': 'foo.herokuapp.com',
         }
+        
+        # structlog version
+        # wrap structlog around a regular logger so that zope's logging support 
+        # still gets the logged messages
+        response = HerokuappAccess(
+            self.handler, self.request.registry)(self.request)
+        assert not self.handler.called, 'handler should not be called'
+        self.assertEqual(len(tweens_handler.records), 1)
+        self.assertEqual(
+            'Herokuapp access denied', # noqa
+            tweens_handler.records[0].msg,
+        )
+        self.assertEqual(response.status_code, 403)
 
+        # standard logging version
+        self.request.registry.settings['pyramid_heroku.structlog'] = False
+        tweens_handler.clear()
         response = HerokuappAccess(
             self.handler, self.request.registry)(self.request)
         assert not self.handler.called, 'handler should not be called'
